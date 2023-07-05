b840581a3f2702157ad7604e832791af49028c34
AJinaEugen <alexandru.jina@gmail.com>
2023-07-05 18:18:58 +0300

    Added js functions to transform and display objects

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 import "./style.css";
 
 console.log("start");
+
+function getTeamAsHtml(team) {
+  return `<tr>
+              <td><input type="checkbox" name="" id="" /></td>
+              <td>${team.Promotion}</td>
+              <td>${team.Members}</td>
+              <td>${team.ProjectName}Native</td>
+              <td>${team.Url}</td>
+              <td>X</td>
+            </tr>`;
+}
+
+function renderTeams(teams) {
+  const htmlTeams = teams.map(getTeamAsHtml);
+  const table = document.querySelector("tbody");
+  table.innerHTML = htmlTeams.join("");
+}
+
+function loadTeams() {
+  fetch("teams.json")
+    .then(r => r.json())
+    .then(renderTeams);
+}
+
+loadTeams();
